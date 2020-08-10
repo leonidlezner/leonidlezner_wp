6974031b3c5db6d9e43b01e8d6e2543ee003abde
Leonid Lezner <kontakt@lezner.de>
2020-08-10 17:37:08 +0200

    Fixing the tags display in the sidebar2

--- yarpp-template-sidebar.php
+++ yarpp-template-sidebar.php
@@ -10,11 +10,15 @@ Description: Template used for the sidebar
 	<?php while (have_posts()) : the_post(); ?>
 	<li>
 		<div class="post-thumbnail">
-		<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail('large'); ?></a>
-        	<?php the_tags('<ul class="tags"><li>', '</li><li>', '</li></ul>'); ?>
+			<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail('large'); ?></a>
 		</div>
+		
 		<h4 class="headline"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+		
 		<?php the_excerpt(); ?>
+		
+		<?php the_tags('<ul class="tags"><li>', '</li><li>', '</li></ul>'); ?>
+		
 	</li>
 	<?php endwhile; ?>
 </ul>
